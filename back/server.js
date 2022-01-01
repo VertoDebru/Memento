5b164ada2145bad776c6b046b409e91faf63b0d9
VertoDebru <t.vervoot@gmail.com>
2022-01-01 17:05:49 +0100

    Change port server.

--- back/server.js
+++ back/server.js
@@ -9,7 +9,7 @@ server.on('error', (err) => {
 });
 server.on('listening', () => {
     const urlServer = server.address();
-    const portServer = '3000';
+    const portServer = server.address().port;
     console.log(`Server On | ${urlServer}:${portServer}`);
 });
 
